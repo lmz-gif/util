9f69d691ad5fdc720e0ed3fc19b4c0cac6469183
lmz-gif <1139138495@qq.com>
2019-10-11 16:29:53 +0800

    once modify

--- src/main/java/com/lmz/utils/LmzUtil.java
+++ src/main/java/com/lmz/utils/LmzUtil.java
@@ -1,4 +1,11 @@
 package com.lmz.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
   * @author lmz
   * @date 2019年10月11日 下午1:33:12
@@ -6,5 +13,45 @@ package com.lmz.utils;
   */
 
 public class LmzUtil {
-
+	//	非空判断
+	public static boolean isEmpty(String str) {
+		return !(str==null||str.trim().length()==0);
+	}
+	
+	//	手机号验证
+	public static boolean cellphoneNumber(String number) {
+		String regex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(number);
+		return matcher.matches();
+	}
+	
+	//	邮箱验证
+	public static boolean isEmail(String email) {
+		String regex = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	
+	//	全字母验证
+	public static boolean isEnglish(String str) {
+		return str.matches("[A-z]+");
+	}
+	
+	//	年龄
+	public static void getAge(String str) {
+		  
+	}
+	
+	//初始化时间
+	public static Date getInitData(String str) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd,hh:mm:ss");
+		Date date = sdf.parse(str);
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM");
+		String format = sdf1.format(date);
+		Date date1 = sdf1.parse(format);
+		return date1;
+	}
+	
 }
---
+++ src/test/java/com/test/Alltest.java
@@ -0,0 +1,22 @@
+package com.test;
+
+
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+import com.lmz.utils.LmzUtil;
+
+/**
+  * @author lmz
+  * @date 2019年10月11日 下午3:08:29
+  * 
+  */
+
+public class Alltest {
+	@Test
+	public void aa() throws ParseException {
+		System.out.println(LmzUtil.getInitData("1998-09-07,01:20:33"));
+	}
+}
